1b681bb22c82ed0ba5dd21d20b15b82f180c94c4
Claribel <Claribel.D.Mendez@gmail.com>
2022-06-16 15:16:43 -0400

    dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnetNewProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
