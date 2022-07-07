82cc4eb9edb5dec54fc2b57018481fd20620f513
Claribel <Claribel.D.Mendez@gmail.com>
2022-07-07 15:20:43 -0400

    Refactor players to model folder

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -54,14 +54,7 @@ public class HomeController : Controller
     }
 
 
-    public class Player
-    {
-        public int PlayerID { get; set; }
-        public string? PlayerFirstName { get; set; }
-        public int Age { get; set; }
-        public object? ViewBag { get; set; }
-        public string? TotalPlayers { get; set; }
-    }
+
 
 
 
