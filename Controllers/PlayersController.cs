9e2ff96838556570a2fc480e68db33343d388bd5
Claribel <Claribel.D.Mendez@gmail.com>
2022-07-07 15:42:05 -0400

    Migrated leaderboard from HomeController to PlayersController

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -1,4 +1,5 @@
 
+using dotnetNewProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -6,32 +7,34 @@ namespace PlayersList;
 
 public class PlayersController : Controller
 {
+    // GET /players/leaderboard
+    [HttpGet]
+    public ActionResult Leaderboard()
+    {
+        IList<Player> playersList = new List<Player>() {
 
-//     public class Player
-//     {
-//         public int PlayerID { get; set; }
-//         public string? PlayerFirstName { get; set; }
-//         public int Age { get; set; }
-    
+                new Player(){ PlayerID=1, PlayerFirstName="Steve", Age = 21 },
+                new Player(){ PlayerID=2, PlayerFirstName="Bill", Age = 25 },
+                new Player(){ PlayerID=3, PlayerFirstName="Ram", Age = 20 },
+                new Player(){ PlayerID=4, PlayerFirstName="Ron", Age = 31 },
+                new Player(){ PlayerID=5, PlayerFirstName="Rob", Age = 19 }
+            };
 
-//         IList<Player> playersList = new List<Player>() {
+        ViewData["players"] = playersList;
 
-//                 new Player(){ PlayerID=1, PlayerFirstName="Steve", Age = 21 },
-//                 new Player(){ PlayerID=2, PlayerFirstName="Bill", Age = 25 },
-//                 new Player(){ PlayerID=3, PlayerFirstName="Ram", Age = 20 },
-//                 new Player(){ PlayerID=4, PlayerFirstName="Ron", Age = 31 },
-//                 new Player(){ PlayerID=5, PlayerFirstName="Rob", Age = 19 }
-//             };
-//     }
-    
+        return View(); //the view for *this* controller / action combo
+        // Views/Players/Leaderboard.cshtml
 
-//     public ActionResult Leaderboard()
+        // return View("~/Views/Players/Leaderboard.cshtml");
+        // explicit - specifying manually - written it out
+        //   pro - little more obvious / intuitive / easier to follow
+        //
+        // return View();
+        // implicit - specified by convention - not written anywhere / magic
+        // use implicit when there's a really strong a convention
+        // pro - saves typing, less noisy
 
-//    {
-//             ViewBag.Message = "Hello";
+    }
 
-//             return View();
-//         }
 
-    
 }
