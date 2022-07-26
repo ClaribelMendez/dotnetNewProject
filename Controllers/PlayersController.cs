708d0b26161455232ebc63b7da5f036457b8be19
Claribel <Claribel.D.Mendez@gmail.com>
2022-07-26 10:24:45 -0400

    Passed model to view

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -14,28 +14,10 @@ public class PlayersController : Controller
             new Player(2, "Ray", "O'Sun", 25, (Teams)2),
             new Player(3, "Rod", "Knee", 20, (Teams)3),
             new Player(4, "Penelope", "Cruz", 31, (Teams)1)
-
-
-                // new Player(){ PlayerID=1, FirstName="Olive", LastName="Tree", Age = 21 },
-                // new Player(){ PlayerID=2, FirstName="Ray", LastName="O'Sun", Age = 25 },
-                // new Player(){ PlayerID=3, FirstName="Rod", LastName="Knee", Age = 20 },
-                // new Player(){ PlayerID=4, FirstName="Penelope", LastName="Cruz", Age = 31 },
             };
+        // var Model = playersList;
 
-        ViewData["players"] = playersList;
-
-        return View(); //the view for *this* controller / action combo
-        // Views/Players/PlayerInfo.cshtml
-
-        // return View("~/Views/Players/PlayerInfo.cshtml");
-        // explicit - specifying manually - written it out
-        //   pro - little more obvious / intuitive / easier to follow
-        //
-        // return View();
-        // implicit - specified by convention - not written anywhere / magic
-        // use implicit when there's a really strong a convention
-        // pro - saves typing, less noisy
-
+        return View(playersList); 
     }
 
 }
