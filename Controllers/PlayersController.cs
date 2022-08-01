8e1f7d477c9203e19bef125e6439191e18c5932a
Claribel Mendez <cmendez@stackoverflow.com>
2022-08-01 15:18:48 -0400

    Created profile view

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -6,18 +6,24 @@ namespace PlayersList;
 
 public class PlayersController : Controller
 {
-    // GET /players/PlayerInfo
-    public ActionResult PlayerInfo()
-    {
-        var playersList = new List<Player>() {
+     List<Player> playersList = new List<Player>() {
             new Player(1, "Olive", "Tree", 21, (Teams)1),
             new Player(2, "Ray", "O'Sun", 25, (Teams)2),
             new Player(3, "Rod", "Knee", 20, (Teams)3),
             new Player(4, "Penelope", "Cruz", 31, (Teams)1)
             };
-        // var Model = playersList;
 
+    // GET /players/PlayerInfo
+    public ActionResult PlayerInfo()
+    {
+       
         return View(playersList); 
     }
 
+    public ActionResult Profile(int id)
+    {
+        var player = playersList.FirstOrDefault( x=> x.PlayerID == id);
+
+        return View(player);
+    }
 }
---
+++ Models/Teams.cs
@@ -0,0 +1,13 @@
+// using System.ComponentModel.DataAnnotations;
+// namespace dotnetNewProject.Models;
+
+
+
+
+// public enum Teams
+// {
+//     Unknown = 0,
+//     Creators = 1,
+//     Discover = 2,
+//     Reach = 3
+// }
