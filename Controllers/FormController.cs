5141e5e77349680b603eb85d0b01117fcb842288
Claribel Mendez <cmendez@stackoverflow.com>
2022-08-08 21:32:03 -0400

    added some user validation

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -13,23 +13,21 @@ public class FormController : Controller
         return View();
     }
 
-    // [
-    //     'key': value
-    //     'FirstName': kyle
-    //     'LastName': mit
-    //     id: 7
-    // ]
     [HttpPost]
     public ActionResult Login(Player newPlayer)
     {
+        if (ModelState.IsValid)
+        {
+            TempData["lastName"] = newPlayer.LastName;
+            TempData["firstName"] = newPlayer.FirstName;
+            TempData["age"] = newPlayer.Age;
+            TempData["team"] = newPlayer.Team;
+        
 
-        // NOTE: (string LastName)
-        TempData["lastName"] = newPlayer.LastName;
-        TempData["firstName"] = newPlayer.FirstName;
-        TempData["age"] = newPlayer.Age;
-        TempData["team"] = newPlayer.Team;
-
+        
         return RedirectToAction("PlayerInfo", "Players");
+    }
+    return View();
     }
 
 }
--- Models/Player.cs
+++ Models/Player.cs
@@ -7,9 +7,14 @@ public class Player
     // [StringLength(4,ErrorMessage="Name must not be more than 4 char")]
     public int PlayerID { get; set; }
     [Required]
+    // [StringLength(8, ErrorMessage = "Name cant be more than 8.")]
     public string? FirstName { get; set; } 
+    [Required]
     public string? LastName { get; set; }
+    [Required]
+    [Range(18,100)]
     public int Age { get; set; }
+    [Required]
     public Teams Team { get; set; }
     
 
