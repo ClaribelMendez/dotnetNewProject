392684376c01ac1c9237fd712322933b22c83d49
ClaribelMendez <95108034+ClaribelMendez@users.noreply.github.com>
2022-08-12 11:12:05 -0400

    Add User profile page view (#8)
    * Passed model to view
    
    * Created profile view
    
    * Added profile details
    
    * Added obj to gitignore
    
    Co-authored-by: Claribel Mendez <cmendez@stackoverflow.com>

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -6,36 +6,24 @@ namespace PlayersList;
 
 public class PlayersController : Controller
 {
-    // GET /players/PlayerInfo
-    public ActionResult PlayerInfo()
-    {
-        var playersList = new List<Player>() {
+     List<Player> playersList = new List<Player>() {
             new Player(1, "Olive", "Tree", 21, (Teams)1),
             new Player(2, "Ray", "O'Sun", 25, (Teams)2),
             new Player(3, "Rod", "Knee", 20, (Teams)3),
             new Player(4, "Penelope", "Cruz", 31, (Teams)1)
-
-
-                // new Player(){ PlayerID=1, FirstName="Olive", LastName="Tree", Age = 21 },
-                // new Player(){ PlayerID=2, FirstName="Ray", LastName="O'Sun", Age = 25 },
-                // new Player(){ PlayerID=3, FirstName="Rod", LastName="Knee", Age = 20 },
-                // new Player(){ PlayerID=4, FirstName="Penelope", LastName="Cruz", Age = 31 },
             };
 
-        ViewData["players"] = playersList;
-
-        return View(); //the view for *this* controller / action combo
-        // Views/Players/PlayerInfo.cshtml
-
-        // return View("~/Views/Players/PlayerInfo.cshtml");
-        // explicit - specifying manually - written it out
-        //   pro - little more obvious / intuitive / easier to follow
-        //
-        // return View();
-        // implicit - specified by convention - not written anywhere / magic
-        // use implicit when there's a really strong a convention
-        // pro - saves typing, less noisy
-
+    // GET /players/PlayerInfo
+    public ActionResult PlayerInfo()
+    {
+       
+        return View(playersList); 
     }
 
+    public ActionResult Profile(int id)
+    {
+        var player = playersList.FirstOrDefault( x=> x.PlayerID == id);
+
+        return View(player);
+    }
 }
